8165c1ef72710ab2834a7a48c019bd34341279af
Raman Shrestha <raman.shrestha@berrybytes.com>
2021-08-17 12:13:32 +0545

    call back as function type

--- app.ts
+++ app.ts
@@ -2,6 +2,13 @@ const add = (number1 : number, number2: number) : number => {
     return number1 + number2
 }
 
+const addHandle = (no1 : number, no2: number, cb: (no3: number) => void) => {
+    let res = no1 + no2
+    cb(res)
+}
+
+addHandle(14,27, (number) => {console.log(number)})
+
 let printResult: (a: number, b: number) => number;
 
 printResult = add
