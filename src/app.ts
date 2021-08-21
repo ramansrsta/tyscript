e4d5cf1ff5a3d95ff7b2d6f5a957f0c005d50053
Raman Shrestha <raman.shrestha@berrybytes.com>
2021-08-21 13:21:38 +0545

    generic functions

--- src/app.ts
+++ src/app.ts
@@ -1,35 +1,12 @@
-interface Bird {
-    type: 'bird',
-    flyingSpeed: number
+function merge<P extends object, A extends object> (obj1: P, obj2: A){
+    return Object.assign(obj1, obj2)
 }
 
-interface Horse {
-    type: 'horse',
-    runningSpeed: number
+const objComb = merge({name: "Ram"}, {age: "23"})
+console.log(objComb.name)
+
+function extractandprint<T extends object, U extends keyof T> (obj: T, key: U){
+    return 'Value is ' + obj[key]
 }
 
-type Animal = Bird | Horse;
-
-function animal(animal: Animal){
-    let speed: number;
-    switch(animal.type){
-        case 'bird':
-            speed = animal.flyingSpeed
-            break;
-        case 'horse':
-            speed = animal.runningSpeed
-            break;
-    }
-    console.log('My speed is : ', speed)
-}
-
-animal({type: 'bird', flyingSpeed: 10})
-
-interface ErrorContainer {
-    [props: string] : string
-}
-
-const ErrorType: ErrorContainer = {
-    name: "Error in name",
-    email: "Email in Errro"
-}
+console.log(extractandprint({name: 'Raja'}, 'name'))
