87a21f6e3867669a5352f83693aa27184a1ec9e8
Raman Shrestha <raman.shrestha@berrybytes.com>
2021-08-31 12:30:55 +0545

    Decorators

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,71 @@
-const person: Readonly<string[]> = ['Ram', 'Anna']
+function Logger(loggerText: string){
+    return function(constructor: Function)
+    {
+        console.log(loggerText)
+        console.log(constructor)
+    }
+}
+@Logger('Logging - Personal Data')
+class Person {
+    name = "Pandu"
+    constructor(){
+        console.log('Creating Class')
+    }
+}
 
-// There are some utility function provided
-// by typescript for users for better type safety
-// and in some case loosing a strict type
-// so in this above utility generic Readonly
-// typescript will give an error if you try to push
-// or pop in the array.
-// there are lots of other function like these you can check documentation
-// provided by typescript
+let per = new Person()
+console.log(per)
+
+//
+
+function Log(target: any, propertyname: string){
+    console.log(target)
+    console.log(propertyname)
+    console.log('OK Tested')
+}
+
+function Log2(target: any, nameOfAccessor: string, descriptor: PropertyDescriptor){
+    console.log(target)
+    console.log(nameOfAccessor)
+    console.log(descriptor)
+    console.log('OK Tested')
+}
+
+
+function Log3(target: any, nameOfMethod: string, descriptor: PropertyDescriptor){
+    console.log(target)
+    console.log(nameOfMethod)
+    console.log(descriptor)
+    console.log('Method')
+}
+
+function Log4(target: any, nameOfMethodInWhichThisParamIsUsed: string, positionOfArgument: number){
+    console.log(target)
+    console.log(nameOfMethodInWhichThisParamIsUsed)
+    console.log(positionOfArgument)
+    console.log('Parameter Decorator')
+}
+class Product {
+    @Log
+    title: string;
+    private _price: number;
+
+    @Log2
+    set price(value: number){
+       if(value > 0){
+           this._price = value
+       }else {
+           throw new Error('Invalid Price')
+       }
+    }
+
+    constructor(t: string, p: number){
+        this.title = t
+        this._price = p
+    }
+
+    @Log3
+    getPriceWithTax(@Log4 tax: number){
+        return this._price * (1 + tax)
+    }
+}
